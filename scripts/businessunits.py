0a02a2d23e1fe3e5930b8e12ba8faef58b9376ee
timur <timurka.zavyalov@mail.ru>
2023-03-17 15:58:54 +0500

    git init

---
+++ scripts/businessunits.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from __init__businessunits import *
+from rn_stagehistories import RnStagehistories
+
+
+class Businessunits(RnStagehistories):
+    pass
+
+
+if __name__ == "__main__":
+    businessunits: Businessunits = Businessunits(os.path.abspath(sys.argv[1]),
+                                                 sys.argv[2], HEADERS_ENG,
+                                                 LIST_OF_FLOAT_TYPE,
+                                                 LIST_OF_BOOL_TYPE,
+                                                 LIST_OF_INT_TYPE, LIST_OF_DATE_TYPE)
+    businessunits.main(__file__)
