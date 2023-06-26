be09397832c90d78a68eeacadc000611b10cec9d
timur <timurka.zavyalov@mail.ru>
2023-06-26 17:34:21 +0300

    fix: fixed types in scripts

--- scripts/__init__processstages.py
+++ scripts/__init__processstages.py
@@ -1,14 +1,14 @@
 HEADERS_ENG: dict = {
     ("@odata.etag",): "odata.etag",
-    ("_ownerid_value,",): "_ownerid_value",
-    ("_processid_value,",): "_processid_value",
-    ("owningbusinessunit,",): "owningbusinessunit",
-    ("stagename,",): "stagename",
-    ("versionnumber,",): "versionnumber",
-    ("primaryentitytypecode,",): "primaryentitytypecode",
-    ("clientdata,",): "clientdata",
-    ("processstageid,",): "processstageid",
-    ("stagecategory,",): "stagecategory"
+    ("_ownerid_value",): "_ownerid_value",
+    ("_processid_value",): "_processid_value",
+    ("owningbusinessunit",): "owningbusinessunit",
+    ("stagename",): "stagename",
+    ("versionnumber",): "versionnumber",
+    ("primaryentitytypecode",): "primaryentitytypecode",
+    ("clientdata",): "clientdata",
+    ("processstageid",): "processstageid",
+    ("stagecategory",): "stagecategory"
 }
 
 LIST_OF_FLOAT_TYPE: list = [
